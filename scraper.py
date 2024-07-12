3bb493564cd40b4dfc1252229557059e6a37284e
Bisky <75712141+Bisky06@users.noreply.github.com>
2024-07-12 23:36:14 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,55 +1,22 @@
-import urllib
 import re
 from urllib.request import urlopen
+from package.data import url, element, split_list
+from package.functions import cleaner
 
-# Following FIFO
 
-url = 'https://www.google.com/search?q=top+search+keywords'
-page = urlopen(url) # opens the url
-
-# detects the web encoding used
+# detects the web encoding used, then reads and converts the webpage into bytes and decodes
+page = urlopen(url)
 encoding = page.headers.get_content_charset() 
-
-# reads and converts the webpage into bytes and decodes
-html_bytes = page.read() 
+html_bytes = page.read()
 html = html_bytes.decode(encoding)
 
-# finding the elements that starts from the <table> container
-c = '<table>'
-c2 = '</table>' 
+# finding the elements that starts from the specified element from the decoded html then filter objects into a str to be used as list
+index = html.find(element[0]) 
+start_index = index + len(element[0]) 
+end_index = html.find(element[1]) 
+title = str(html[start_index:end_index])
 
-# finds the element index and output to a list
-index = html.find(c) 
-start_index = index + len(c) 
-end_index = html.find(c2) 
-title = html[start_index:end_index] 
+output_list = str(re.findall('<div class="BNeawe s3v9rd AP7Wnd">.*</div>', title))
 
-title = str(title)
-
-# outputs a list with everything on one index
-f = re.findall('<div class="BNeawe s3v9rd AP7Wnd">.*</div>', title)
-
-f = str(f)
-
-# splitting objects and sorting into a list
-split_list = ['<div class="BNeawe s3v9rd AP7Wnd">','</div>','<div class="BNeawe s3v9rd AP7Wnd">']
-
-n = []
-
-for i in split_list:
-    n = f.split(i)
-
-for i in n:
-    if i == '':
-        n.remove(i)
-
-value = []
-
-for i in n:
-    if ' ' in i or ',' in i:
-        i = i.split('</div>')
-        i.pop(1)
-        i.pop(1)
-        value += i
-
-print (value)
+# cleans the output
+end_result = cleaner(output_list,split_list)
